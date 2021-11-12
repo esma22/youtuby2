69796e417c85f534f62cacfabd58094a806547ea
Asmaaecommerce <Asmaaecommerce@gmail.com>
2021-11-12 10:55:50 +0200

    youtuby2

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $(".first-list-element").addClass("active");
+    //لحذف الصنف النشط، ووضعه عندما يُضغط على أحد أزرار القائمة العلوية
+    $(".nav-item").click(function () {
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");
+    });
+    //عند الضغط على الأيقونة الخاصة بالقائمة الجانبية
+    $("#toggler").click(function(event) {
+        $('#wrap').toggleClass('toggled');
+        var right = $('.sidebar').css("right"); //أعطينا هذه الخاصية للقائمة الجانبية لكي تُصبح على اليمين
+        if (right == '0px') //القائمة الجانبية بحالة إظهار وبالتالي يجب إغلاقها
+        {
+            $('.sidebar').css({ 'right': '-17rem' });
+            $('.layer').fadeOut(); /*أخفي الطبقة السوداء*/
+        }
+        else { //القائمة الجانبية بحالة إغلاق وبالتالي يجب إظهارها
+            $('.sidebar').css({ 'right': '0' });
+            $('.layer').fadeIn(); /*أظهر الطبقة السوداء*/
+        } 
+        $('.layer').click(function(){
+            $('.sidebar').css({'right' :'-17rem' });
+            $('.layer').fadeOut();
+        });
+    });
+     $(".search-icon").click(function(){
+         $(".search-input").slideToggle("slow");
+     });
+     
+});
---
+++ js/search.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#search-filters-icon").click(function(){
+        $(".filter-column").slideToggle("siow");
+    });
+    $("input:checkbox").click(function () {
+        var $box = $(this);//تحديد صندوق التأشير الذي تم الضغط عليه
+
+        if ($box.is(":checked")) {//إذا حدد صندوق التأشير؟
+            //attr الحصول على اسم صندوق التأشير عن طريق الخاصية
+            var group = "input:checkbox[name='" + $box.attr("name") + "']";
+            $(group).prop("checked", false);//تحديد خطأ لجميع صناديق التأشير الموجودة في المجموعة التي لها الاسم الذي جلبناه في السطر السابق
+            $box.prop("checked", true);//تحديد صح فقط لصندوق التأشير الذي ضغطنا عليه
+        }
+    });
+});
